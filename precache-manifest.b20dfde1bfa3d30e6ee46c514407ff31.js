d5bdc731b8a0bd8f6c276cacbb1c88e20de791a6
mwai <mwaijohng@gmail.com>
2020-07-15 15:49:42 +0300

    Updates

---
+++ precache-manifest.b20dfde1bfa3d30e6ee46c514407ff31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f763b91fc242daacd4ec5d78bbda581",
+    "url": "/local_storage/index.html"
+  },
+  {
+    "revision": "15d22025adeeb24ae08b",
+    "url": "/local_storage/static/css/main.f2a985cc.chunk.css"
+  },
+  {
+    "revision": "c4f753cecc0187365beb",
+    "url": "/local_storage/static/js/2.d1902a06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/local_storage/static/js/2.d1902a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d22025adeeb24ae08b",
+    "url": "/local_storage/static/js/main.db3fe8fb.chunk.js"
+  },
+  {
+    "revision": "5c2fdbd11c7124b264e5",
+    "url": "/local_storage/static/js/runtime-main.9a1a3674.js"
+  }
+]);
